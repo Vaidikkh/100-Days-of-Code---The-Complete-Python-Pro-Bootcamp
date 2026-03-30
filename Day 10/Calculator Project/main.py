2b0277f5df6596d43ca37f9cfa409bd3beed2e6a
Vaidik Khandelwal <Khandelwalvaidik122333@gmail.com>
2026-03-30 23:24:07 +0530

    making a calculator

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,44 @@
+
+import art
+
 def add(n1, n2):
     return n1 + n2
 
+def sub(n1,n2):
+    return n1 - n2
 
+def mul(n1, n2):
+    return n1*n2
+def div(n1,n2):
+    return n1/n2
+
+
+operations = {
+    "+":add,
+    "-":sub,
+    "*":mul,
+    "/":div,
+}
+def calculate():
+    print(art.logo)
+    should_accumulate = True
+    num1 = float(input("first number"))
+
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        operation = input("Pick an Operation")
+        num2 = float(input("second number"))
+        answer = operations[operation](num1, num2)
+        print(f"{num1} {operation} {num2}= {answer}")
+
+        choice = input(f"Type 'y  to continue calculating with{answer}, or type 'n ' to start a new calculation")
+
+        if choice == "y":
+            num1 = answer
+        else :
+            should_accumulate = False
+            print("\n" * 20)
+            calculate()
+
+calculate()
--- Day 10/Functions with Outputs/task.py
+++ Day 10/Functions with Outputs/task.py
@@ -1,7 +1,20 @@
 def format_name(f_name,l_name):
+    if f_name =="" or l_name =="":
+        return  "You did not provide the inputs"
     formatted_f_name = f_name.title()
     formatted_l_name = l_name.title()
-    print(f"{formatted_f_name} {formatted_l_name}")
+    return f"{formatted_f_name} {formatted_l_name}" # the return comes in functoion means that it is the end of the function if therer is any code after return than that's not gonna execute
+    # print("hii") # this code is unreachable
 
-print(format_name("VAidIk", "KhAnDElWal"))
+print(format_name(input("What's your First name?"), input("What's your last name?")))
+"""
+def function_1(text):
+    return text +text
+
+def function_2(text):
+    return text.title()
+
+op = function_2(function_1("helLo"))
+print(op)"""
+
 
